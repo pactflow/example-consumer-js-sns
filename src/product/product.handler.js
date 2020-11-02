0bea9ac628e65945e05512aa90141e41a94ce9ad
Matt Fellows <matt.fellows@onegeek.com.au>
2020-11-02 18:21:19 +1100

    chore: tidy up handler

--- src/product/product.handler.js
+++ src/product/product.handler.js
@@ -1,9 +1,10 @@
 const repository = require("./product.repository");
-const Product = require('./product');
+const Product = require("./product");
 
 // Actual message handler, doesn't care about SNS at all!
-const handler = (product) => {
-  Promise.resolve(repository.insert(new Product(product.id, product.type, product.name)))
-}
+const handler = (product) =>
+  Promise.resolve(
+    repository.insert(new Product(product.id, product.type, product.name))
+  );
 
-module.exports = {handler}
+module.exports = { handler };
