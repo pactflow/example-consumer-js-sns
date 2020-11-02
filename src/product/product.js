2ea28e2d1f92568798c515c1d419271d93f22ff6
Matt Fellows <matt.fellows@onegeek.com.au>
2020-11-02 18:36:48 +1100

    wip: more cleanup

--- src/product/product.js
+++ src/product/product.js
@@ -1,15 +1,15 @@
-const assert = require('assert').strict
+const assert = require("assert").strict;
 
 class Product {
-    constructor(id, type, name) {
-        assert(id, "id is a mandatory field")
-        assert(type, "type is a mandatory field")
-        assert(name, "name is a mandatory field")
+  constructor(id, type, name) {
+    assert(id, "id is a mandatory field");
+    assert(type, "type is a mandatory field");
+    assert(name, "name is a mandatory field");
 
-        this.id = id;
-        this.type = type;
-        this.name = name;
-    }
+    this.id = id;
+    this.type = type;
+    this.name = name;
+  }
 }
 
 module.exports = Product;
