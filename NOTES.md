b1607c6bee3f8b4781ddded025ae1f19070cb231
Yousaf Nabi <yousaf@pactflow.io>
2023-10-10 18:40:03 +0100

    chore: workshop updates

---
+++ NOTES.md
@@ -0,0 +1,11 @@
+# Workshop WIP notes
+
+1. Instruct users to install [LocalStack CLI](https://docs.localstack.cloud/getting-started/installation/#localstack-cli), to test e2e locally with Docker
+   1. [Alternative install mechanisms](https://docs.localstack.cloud/getting-started/installation/#alternatives) 
+2. Install [aws-local cli](https://docs.localstack.cloud/user-guide/integrations/aws-cli/#localstack-aws-cli-awslocal)
+3. Install [aws-sam-cli-local](https://docs.localstack.cloud/user-guide/integrations/aws-sam/)
+4. Need Docker
+5. Start LocalStack `localstack start -d` (detached mode) remove `-d`  to tail logs (will need more than 1 terminal or tabs)
+6. Add PactBroker config
+7. Update Provider side
+8. Show delta between pact ruby core and pact rust core generated Pact files.
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,14 @@
 set -e
 
 echo "finding topic"
-TOPIC_ARN=$(aws sns list-topics --output json | grep pactflow-example-consumer-js-sns-ProductEvent | cut -d'"' -f4)
+export REGION=us-east-1
+TOPIC_ARN=$(awslocal sns list-topics --output json --region us-east-1 | grep topic| cut -d'"' -f4)
 echo "have topic: ${TOPIC_ARN}, publishing message"
-aws sns publish --topic-arn "${TOPIC_ARN}" --message "{\"id\": \"some-uuid-1234-5678\", \"type\": \"Product Range\", \"name\": \"Some Product\", \"event\": \"UPDATED\" }"
+TOPIC_ARN=arn:aws:sns:ap-southeast-2:000000000000:topic-447de61c 
+awslocal sns publish --region us-east-1 --topic-arn "${TOPIC_ARN}" --message "{\"id\": \"some-uuid-1234-5678\", \"type\": \"Product Range\", \"name\": \"Some Product\", \"event\": \"UPDATED\" }"
 npm run logs
+
+
+export AWS_REGION=ap-southeast-2 
+awslocal cloudformation list-stacks --region us-east-1
+samlocal logs -n ProductEventHandler --stack-name pactflow-example-consumer-js-sns -t
